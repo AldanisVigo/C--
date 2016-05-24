91dca0c63f3530c2772b548dfbbcdff98c804b71
Aldanis Vigo <aldanisvigo@Aldaniss-MacBook-Pro.local>
2016-05-24 11:22:20 -0500

    C++ Curl Example"

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "curly.h"
+
+int main(int argc, const char* argv[]){
+	Curly curly;
+	if(curly.Fetch("http://www.google.com") == CURLE_OK){
+		std::cout << "status:" << curly.HttpStatus() << std::endl;
+		std::cout << "type:" << curly.Type() << std::endl;
+		std::vector<std::string> headers = curly.Headers();
+
+		for(std::vector<std::string>::iterator it = headers.begin();it  != headers.end();it++)
+			std::cout << "Header:" << (*it) << std::endl;
+
+		std::cout << "Content:\n" << curly.Content() << std::endl;	
+	}
+	return 0;
+}
