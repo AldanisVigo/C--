33ac5a32d36bc1eb40ff5a63af61d2b9f814103d
Aldanis Vigo <aldanisvigo@gmail.com>
2016-05-24 23:45:42 -0500

    Added MySQL Example

---
+++ MySQL/Test/main.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <iostream>
+#include <mysql_connection.h>
+#include <driver.h>
+#include <exception.h>
+#include <resultset.h>
+#include <statement.h>
+using namespace std;
+using namespace sql;
+int main(void){
+	sql::Driver *driver;
+ 	sql::Connection *con;
+ 	sql::Statement *stmt;
+	sql::ResultSet *res;
+
+ 	driver = get_driver_instance();
+  	con = driver->connect("tcp://127.0.0.1:3306","root","ilovejenn2");
+	
+	stmt = con->createStatement();
+	stmt->execute("USE test");
+	res = stmt->executeQuery("SELECT * FROM tbl1");
+	while(res->next()){
+		cout << "Your name is :" << res->getString("Name") << " " << res->getString("LastName") << endl;
+	}
+  return 0;
+}
